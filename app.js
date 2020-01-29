ab51033df37b4e273f95ed8d3ff4745ea02a58c5
Vikram <58028680+bcalm@users.noreply.github.com>
2020-01-29 15:24:19 +0530

    Made: appClass.
    Applied: appClass in server

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const {App} = require('./lib/appClass');
 const queryString = require('querystring');
 const contentTypes = require('./lib/mimeTypes.js');
 
 const STATIC_FOLDER = `${__dirname}/public`;
 
-const fileNotFound = function() {
+const fileNotFound = function(req, res, next) {
   const content = `<html>
     <head>
       <title>NOT FOUND</title>
@@ -15,22 +16,31 @@ const fileNotFound = function() {
     </body>
   </html>`;
   const statusCode = 404;
-  const contentType = 'text/html';
-  return {statusCode, content, contentType};
+  res.writeHead(statusCode, {'content-type': 'text/html'});
+  res.write(content);
+  res.end();
 };
 
-const serveStaticFile = req => {
+const isFileExists = function(path) {
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  return !stat || !stat.isFile();
+};
+
+const serveStaticFile = (req, res, next) => {
   req.url = req.url === '/' ? '/html/index.html' : req.url;
   const path = `${STATIC_FOLDER}${req.url}`;
-  const stat = fs.existsSync(path) && fs.statSync(path);
-  if (!stat || !stat.isFile()) {
-    return fileNotFound();
+  if (isFileExists(path)) {
+    console.log('inside serveStaticFile');
+    next();
+    return;
   }
   const [, extension] = path.match(/.*\.(.*)$/) || [];
   const contentType = contentTypes[extension];
   const content = fs.readFileSync(path);
   const statusCode = 200;
-  return {statusCode, content, contentType};
+  res.writeHead(statusCode, {'content-type': contentType});
+  res.write(content);
+  res.end();
 };
 
 const createTable = function(commentDetails, commentList) {
@@ -53,49 +63,52 @@ const getComment = function() {
   return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 };
 
-const serveGuestPage = function(req) {
+const serveGuestPage = function(req, res, next) {
   const commentFile = getComment();
   const path = `${STATIC_FOLDER}${req.url}`;
+  if (isFileExists(path)) {
+    next();
+    return;
+  }
   const guestBook = fs.readFileSync(path, 'utf8');
   const allComment = commentFile.reduce(createTable, '');
   const content = guestBook.replace(/__comment__/, allComment);
   const contentType = 'text/html';
   const statusCode = 200;
-  return {statusCode, content, contentType};
+  res.writeHead(statusCode, {'content-type': contentType});
+  res.write(content);
+  res.end();
 };
 
-const updateComment = function(req, data) {
+const updateComment = function(req, res, next) {
   const path = './dataBase/commentList.json';
   const commentFile = getComment();
   const date = new Date();
-  const commentDetail = queryString.parse(data);
+  const commentDetail = queryString.parse(req.body);
   const name = commentDetail.name;
   const comment = commentDetail.comment;
   commentFile.unshift({name, date, comment});
   fs.writeFileSync(path, JSON.stringify(commentFile));
-  return serveGuestPage(req);
+  return serveGuestPage(req, res, next);
 };
 
-const getHandlers = {
-  '/html/guestBook.html': serveGuestPage,
-  other: serveStaticFile
+const readBody = function(req, res, next) {
+  let data = '';
+  req.on('data', chunk => (data += chunk));
+  req.on('end', () => {
+    req.body = data;
+    next();
+  });
 };
 
-const postHandlers = {
-  '/html/guestBook.html': updateComment
-};
+const app = new App();
 
-const methods = {
-  GET: getHandlers,
-  POST: postHandlers
-};
+app.use(readBody);
+app.get('/html/guestBook.html', serveGuestPage);
+app.get('', serveStaticFile);
+app.get('', fileNotFound);
 
-const getResponse = function(req, data) {
-  const handlers = methods[req.method];
-  const handler = handlers[req.url] || handlers.other;
-  const {statusCode, content, contentType} = handler(req, data);
-  const header = {'content-type': contentType || 'text/html'};
-  return {content, statusCode, header};
-};
+app.post('/html/guestBook.html', updateComment);
+app.post('', fileNotFound);
 
-module.exports = {getResponse};
+module.exports = {app};
--- server.js
+++ server.js
@@ -1,23 +1,8 @@
 const http = require('http');
-const {getResponse} = require('./app');
+const {app} = require('./app');
 
 const port = 80;
 
-const handleRequest = function(req, res) {
-  let data = '';
-  req.setEncoding('utf8');
-  req.on('data', chunk => {
-    data += chunk;
-  });
-
-  req.on('end', () => {
-    const {content, statusCode, header} = getResponse(req, data);
-    res.writeHead(statusCode, header);
-    res.write(content);
-    res.end();
-  });
-};
-
-const server = new http.Server(handleRequest);
+const server = new http.Server(app.serve.bind(app));
 
 server.listen(port);

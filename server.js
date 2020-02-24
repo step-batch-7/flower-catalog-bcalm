3a1f910da7ed6b93ca8b23fcf2137a59a589df6b
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-02-24 16:07:13 +0530

    Modified: port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const {app} = require('./js/handler');
 
-const port = 80;
+const port = process.env.PORT || 80;
 
 const server = new http.Server((req, res) => {
   app.serve(req, res);
